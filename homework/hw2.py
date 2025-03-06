4f2ef4e44b811c393b1738cb56eb4c43467c87b8
Spencer Rak <spencer.rak@gmail.com>
2025-03-05 19:11:11 -0500

    HW2

--- homework/hw2.py
+++ homework/hw2.py
@@ -18,3 +18,41 @@
 # Please see the below image as a sample of what your program could look like. You do
 # not have to specify how many or which character sets are missing. | just wanted to
 # track it during my testing.
+
+# SPDX-FileCopyrightText: © 2025 Spencer Rak
+# SPDX-License-Identifier: MIT
+#!/usr/bin/python3
+
+import getpass
+import string
+
+def main():
+    pw = None
+
+    def find_char(charset, string):
+        return True in list(map(lambda x: x in charset, string))
+
+    print("This is the password checker. Is your password good?\n")
+    while 1:
+        pw = getpass.getpass()
+        if len(pw) < 14:
+            print("Enter 14 or more characters")
+            continue
+        if not find_char(string.ascii_lowercase, pw):
+            print("Your password must contain a lowercase letter")
+            continue
+        if not find_char(string.ascii_uppercase, pw):
+            print("Your password must contain an uppercase letter")
+            continue
+        if not find_char(string.digits, pw):
+            print("Your password must contain a digit")
+            continue
+        if not find_char(string.punctuation, pw):
+            print("Your password must contain a symbol")
+            continue
+        print(f"You entered\n\n`{pw}`\n\nNice password!")
+        break
+        
+
+if __name__ == "__main__":
+    main()

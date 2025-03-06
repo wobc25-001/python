e164a52a076f4ec94a266bf3773ea063ee3ace61
Spencer Rak <spencer.rak@gmail.com>
2025-03-06 07:20:16 -0500

    Move functions

--- homework/hw3.py
+++ homework/hw3.py
@@ -10,80 +10,74 @@
 # - Simple checking OR
 # - Regular expressions
 # - For loops
-# 
-# addresses = (
-# '192.168.254.1",
-# 
-# '867.53.0.9,
-# '192.168.254.1",
-# 
-# '255.255.255.257',
-# '10.10.100.1",
-# "172.16.0.1',
-# '192.168.254.1",
-# 
-# '10.10.100.2,
-# '8.8.8.8/,
-# '"1337.4150.4444.07
-# 
-# )
-# Write a program that uses the above tuple of IP addresses to:
-# 
-# Print a dictionary with each IP address as the key and the count of occurences of each
-# 
-# IP address in the tuple as its value.
-# Evalute each IP address for validity.
-# Print a list of all valid IP address occurences in order of appearance.
-# Print a set of all valid IP addresses.
-# Print a sorted set of all unique valid IP addresses.
+#
 
 # SPDX-FileCopyrightText: © 2025 Spencer Rak
 # SPDX-License-Identifier: MIT
 #!/usr/bin/python3
 
+import ipaddress
 import random
 import string
 
-def roctet():
-    n = random.randint(1, 3)
-    f = None
-    if n > 2:
-        f = str(random.randint(1, 2))
 
-    r = "".join([random.choice(string.digits) for _ in range(0, (2 if n == 3 else n))])
-    if f:
-        r = f+r
-    if int(r) > 255:
-        r = "255"
+def rand_addrs(addrs: int, dupes: int) -> [str]:
+    def mkdupes(addrs, n):
+        dupes = []
+        for _ in range(n):
+            dupes.append(random.choice(addrs))
+        dupes += addrs
+        return dupes
+
+    def roctet():
+        n = random.randint(1, 3)
+        f = None
+        if n > 2:
+            f = str(random.randint(1, 2))
+
+        r = "".join([random.choice(string.digits) for _ in range(0, (2 if n == 3 else n))])
+        if f:
+            r = f+r
+        return r
+
+    def raddr():
+        return ".".join([roctet() for _ in range(4)])
+
+    r = [raddr() for _ in range(addrs)]
+    r = mkdupes(r, dupes)
     return r
 
 
-def raddr():
-    return ".".join([roctet() for _ in range(4)])
-
-def raddrs(x):
-    return [raddr() for _ in range(x)]
-
-def mkdupes(addrs, n):
-    dupes = []
-    for _ in range(n):
-        dupes.append(random.choice(addrs))
-    dupes += addrs
-    return dupes
+def valid_addr(a: str) -> bool:
+    try:
+        _ = ipaddress.IPv4Address(a)
+        return True
+    except ipaddress.AddressValueError as e:
+        return False
 
 def main():
-    addrs = raddrs(100)
-    addrs = mkdupes(addrs, 100)
+    addrs = rand_addrs(addrs=200, dupes=100)
 
     sort = {}
+    vaddrs = []
 
     for a in addrs:
+        # Evalute each IP address for validity.
+        if not valid_addr(a):
+            #print(f"`{a}` is not valid")
+            continue
+        vaddrs.append(a)
         if a in sort.keys():
             sort[a] += 1
         else:
             sort[a] = 1
 
-    print(sort)
+    # Print a list of all valid IP address occurences in order of appearance.
+    print(vaddrs)
+    # Print a set of all valid IP addresses.
+    print(set(x for x in sort.keys()))
+    # Print a sorted set of all unique valid IP addresses.
+    print(sorted(set(x for x in sort.keys())))
 
 
 
